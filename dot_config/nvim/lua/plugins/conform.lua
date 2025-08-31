8afea1c9f1fde6ba83602252fefe938f2a36dd48
Kevin Christ <christkevin@gmail.com>
2025-08-31 21:05:14 +0200

    actually fixed the problem with the link

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				css = { "prettier" },
+				go = { "gofmt" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
+	end,
+}
